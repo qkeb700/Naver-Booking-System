9b84878fcd54b4b47deae4bcf1d963691a1a06d5
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-01-10 02:08:48 +0900

    build: Make category dao
    Set up dao and sql for dao.

---
+++ src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
@@ -0,0 +1,26 @@
+package kr.or.connect.reservation.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import kr.or.connect.reservation.dto.Category;
+
+@Repository
+public class CategoryDao {
+	private NamedParameterJdbcTemplate jdbc;
+	private RowMapper<Category> rowMapper = BeanPropertyRowMapper.newInstance(Category.class);
+	
+	public CategoryDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	public List<Category> selectAll(){
+		return jdbc.query(CategoryDaoSql.SELECT_CATEGORY_LIST, rowMapper);
+	}
+}
---
+++ src/main/java/kr/or/connect/reservation/dao/CategoryDaoSql.java
@@ -0,0 +1,5 @@
+package kr.or.connect.reservation.dao;
+
+public class CategoryDaoSql {
+	public static final String SELECT_CATEGORY_LIST = "dk";
+}
