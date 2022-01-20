177e7da09050e8ff6840d0305f714da79cb23ad1
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-01-20 23:02:03 +0900

    feat : 상품 API로 메인페이지 구현
    Ajax를 이용하여 비동기로 데이터를 가져와 메인페이지 구현.

--- src/main/webapp/resources/js/mainpage.js
+++ src/main/webapp/resources/js/mainpage.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
 	ajaxImage();
 	imgSlider();
+	clickCategory();
+	ajaxProducts(start);
+	moreBtn();
 })
 
 function ajaxImage(){
@@ -40,7 +43,75 @@ function imgSlider(){
 		index++;
 	}, 2000)
 }
+let start = 0;
+function ajaxProducts(start) {
+	let oReq = new XMLHttpRequest();
+	let productsFromJson;
+	let itemList = document.querySelector("#itemList").innerHTML;
+	let input;
+	let lstEventBox = document.querySelectorAll(".lst_event_box");
+	let totalCount = document.querySelector(".event_lst_txt .pink");
+	let categoryId = document.querySelector(".event_tab_lst .item .anchor.active").parentNode.getAttribute("data-category");
+	
+	oReq.addEventListener("load", function(){
+		productsFromJson = JSON.parse(oReq.responseText);
+		totalCount.innerHTML = productsFromJson.totalCount + '개';
+		
+		for(let key in productsFromJson.items) {
+			input = itemList.replace("{id}", productsFromJson.items[key].displayInfoId)
+							.replace("{description}", productsFromJson.items[key].productDescription)
+							.replace("{productImgUrl2}", productsFromJson.items[key].productImageUrl)
+							.replace("{description}", productsFromJson.items[key].productDescription)
+							.replace("{placeName}", productsFromJson.items[key].placeName)
+							.replace("{content}", productsFromJson.items[key].productContent);
+		
+			if(key % 2 == 0) {
+				lstEventBox[0].innerHTML += input;				
+			} else {
+				lstEventBox[1].innerHTML += input;
+			}
+		}
+	});
+	
+	oReq.open("GET", "/reservation/api/products?categoryId=" + categoryId + "&start=" + start);
+	oReq.send();
+}
+
 
 function clickCategory(){
 	let categoryList = document.querySelector(".event_tab_lst");
+	categoryList.addEventListener("click", function(evt){
+		let targetElem = document.querySelector(".anchor.active");
+		if(targetElem){
+			document.querySelector(".anchor.active").classList.remove("active");		
+		}
+		if(evt.target.tagName == "SPAN") {
+			evt.target.parentNode.classList.add("active");
+			deleteProducts();
+			start = 0;
+			ajaxProducts(start);
+		} else if(evt.target.tagName == "A") {
+			evt.target.classList.add("active");
+			deleteProducts();
+			start = 0;
+			ajaxProducts(start);
+		}
+	});
+}
+
+function deleteProducts(){
+	let lstEventBox = document.querySelectorAll(".lst_event_box");
+	for(let i = 0; i < lstEventBox.length; i++) {
+		while(lstEventBox[i].hasChildNodes()){
+			lstEventBox[i].removeChild(lstEventBox[i].firstChild);
+		}
+	}
+}
+function moreBtn() {
+	let moreBtn = document.querySelector(".more .btn");
+	
+	moreBtn.addEventListener("click", function(){
+		start += 4;
+		ajaxProducts(start);
+	});
 }
