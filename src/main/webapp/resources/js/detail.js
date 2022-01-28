a0e40f20ba32ce0d5e4af7842de6119911052b26
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-01-28 23:42:55 +0900

    feat : API 로 부터 상품들 데이터 가져오기
    Javascript로 ajax를 사용하여 데이터를 불러온다.

--- src/main/webapp/resources/js/detail.js
+++ src/main/webapp/resources/js/detail.js
@@ -2,3 +2,9 @@ document.addEventListener("DOMContentLoaded", function(){
 	
 });
 
+function ajaxProductByDisplayId(displayId) {
+	let oReq = new XMLHttpRequest();
+	
+	oReq.open("GET", "/reservation/api/products?displayId=" + displayId);
+	oReq.send();
+}
