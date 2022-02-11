722d3a7ea5f8ded5318ab71325a09d0683490343
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-02-11 23:43:07 +0900

    feat : ajax 통신과 생성자 구현
    생성자를 구현하고 상품정보를 가져올 수 있는 데이터 통신을 한다.

--- src/main/webapp/resources/js/reserve.js
+++ src/main/webapp/resources/js/reserve.js
@@ -1,10 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
-	
+	let ajaxPage = new AjaxPage();
+	ajaxPage.getApiProduct();
 });
+
+function AjaxPage() {
+	
+}
+AjaxPage.prototype.getApiProduct = function() {
+	let oReq = new XMLHttpReqeust();
+	let currentUrl = window.location.href;
+	const url = new URL(currentUrl);
+	const urlParams = url.searchParams;
+	let displayId = urlParams.get('id');
+	let displayJson;
+	
+	oReq.addEventListener("load", function(){
+		displayJson = JSON.parse(oReq.responseText);
+	})
+	
+	oReq.open("GET", "/reservation/api/products" + displayId, true);
+	oReq.send();
+}
 
 function TicketSelect() {
 	
 }
 TicketSelect.prototype = {
+	ticketQtyChange : function(){
+		let reserveTemplate = document.querySelector("#reserveTemplate");
+		let bindTemplate = Handlebars.compile(reserveTemplate);
+		let ticketChange = document.querySelector(".ticket_body");	
+	}
 	
 }
