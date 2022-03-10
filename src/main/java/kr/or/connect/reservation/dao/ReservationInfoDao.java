518f324a90b88d0ebe19faaf525e8e4c7971ddc3
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-03-10 16:09:34 +0900

    feat : 예약 DAO 관리
    정보를 저장하고 DB에 저장시키기 위해 DAO를 작성한다.

--- src/main/java/kr/or/connect/reservation/dao/ReservationInfoDao.java
+++ src/main/java/kr/or/connect/reservation/dao/ReservationInfoDao.java
@@ -1,8 +1,14 @@
 package kr.or.connect.reservation.dao;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.reservation.dto.ReservationInfo;
@@ -11,4 +17,15 @@ import kr.or.connect.reservation.dto.ReservationInfo;
 public class ReservationInfoDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private RowMapper<ReservationInfo> rowMapper = BeanPropertyRowMapper.newInstance(ReservationInfo.class);
+	private SimpleJdbcInsert insertAction;
+	
+	public ReservationInfoDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		this.insertAction = new SimpleJdbcInsert(dataSource).withTableName("Reservation_info").usingGeneratedKeyColumns("id").usingColumns(
+							"product_id", "display_info_id", "reservation_name", "reservation_tel", "reservation_email", "reservation_date",
+							"create_date", "modify_date");
+	}
+	public List<ReservationInfo> selectByEmail(String reservationEmail) {
+		return jdbc.query(ReservationInfoDaoSql, Collections.singletonMap("reservationEmail", reservationEmail), rowMapper);
+	}
 }
