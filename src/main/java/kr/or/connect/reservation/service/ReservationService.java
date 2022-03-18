742110a37f6bb1592e2085c9ea543e6e19af8042
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-03-18 19:12:00 +0900

    feat : 예약 service 객체 생성
    예약을 할때 DAO, DTO를 사용하기 위한 service 구현.

--- src/main/java/kr/or/connect/reservation/service/ReservationService.java
+++ src/main/java/kr/or/connect/reservation/service/ReservationService.java
@@ -3,11 +3,13 @@ package kr.or.connect.reservation.service;
 import java.util.List;
 
 import kr.or.connect.reservation.dto.ReservationInfo;
+import kr.or.connect.reservation.dto.ReservationInfoPriceDto;
+import kr.or.connect.reservation.dto.ReservationInfoSetDto;
 
 public interface ReservationService {
-	public List<ReservationInfo> getReservationInfoByEmail(String reservationEmail);
-	
-	public int getTotalPrice(String reservationEmail, int productId, int displayInfoId, String reservationDate);
-
-	public int getIdByEmail(String reservationEmail);
+	public ReservationInfoSetDto getReservationInfoSet(String reservationEmail);
+	public ReservationInfoPriceDto addReservation(ReservationInfoPriceDto reservationDto);
+	public ReservationInfoPriceDto changeCancelFlagById(int reservationId);
+	public ReservationInfoPriceDto getReservationInfoById(int reservationId);
+	public boolean existReservation(String reservationEmail);
 }
