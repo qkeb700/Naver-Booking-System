45e789292a3f02b0d5699b97a97b2edd95f9b89d
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-03-25 18:08:03 +0900

    feat : 예약 put 어노테이션 추가
    예약 정보를 취소하는 메서드  구현하기.

--- src/main/java/kr/or/connect/reservation/controller/NaverBookingApiController.java
+++ src/main/java/kr/or/connect/reservation/controller/NaverBookingApiController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -90,6 +91,9 @@ public class NaverBookingApiController {
 	public ReservationInfoPriceDto addReservationInfo(@RequestBody ReservationInfoPriceDto reservationInfoPriceDto) {
 		return reservationService.addReservation(reservationInfoPriceDto);
 	}
-	
+	@PutMapping("/reservations/{reservationId}")
+	public ReservationInfoPriceDto deleteReservationInfo(@PathVariable int reservationId) {
+		return reservationService.changeCancelFlagById(reservationId);
+	}
 	
 }
