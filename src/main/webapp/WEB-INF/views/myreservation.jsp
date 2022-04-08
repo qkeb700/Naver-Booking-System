aed9936194e6fd117a96ea2101927e8e9b9f37b7
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-04-09 02:35:09 +0900

    feat : jsp에 예약 정보 출력
    실제 api에서 가져온 정보를 핸들바를 사용해 jsp에 정보 값 출력하기.

--- src/main/webapp/WEB-INF/views/myreservation.jsp
+++ src/main/webapp/WEB-INF/views/myreservation.jsp
@@ -175,32 +175,29 @@
 <article class="card_item">
 	<a href="#" class="link_booking_details">
 		<div class="card_body">
+			<input type="hidden" id="productId" value={{productId}} />
 			<div class="left"></div>
 			<div class="middle">
 				<div class="card_detail">
-					<em class="booking_number">No.0000000</em>
-					<h4 class="tit">서비스명/상품명</h4>
+					<em class="booking_number">{{reservationInfoId}}</em>
+					<h4 class="tit">{{productDescription}}</h4>
 					<ul class="detail">
 						<li class="item"><span class="item_tit">일정</span> <em
-							class="item_dsc"> 2000.0.00.(월)2000.0.00.(일) </em></li>
+							class="item_dsc"> {{reservationDate}} </em></li>
 						<li class="item"><span class="item_tit">내역</span> <em
-							class="item_dsc"> 내역이 없습니다. </em></li>
+							class="item_dsc"> {{productContent}} </em></li>
 						<li class="item"><span class="item_tit">장소</span> <em
-							class="item_dsc"> 내역이 없습니다. </em></li>
+							class="item_dsc"> {{placeLot}} </em></li>
 						<li class="item"><span class="item_tit">업체</span> <em
-							class="item_dsc"> 업체명이 없습니다. </em></li>
+							class="item_dsc"> {{placeName}} </em></li>
 					</ul>
 					<div class="price_summary">
 						<span class="price_tit">결제 예정금액</span> <em class="price_amount">
-							<span>000,000,000</span> <span class="unit">원</span>
+							<span>{{totalPrice}}</span> <span class="unit">원</span>
 						</em>
 					</div>
 					<!-- [D] 예약 신청중, 예약 확정 만 취소가능, 취소 버튼 클릭 시 취소 팝업 활성화 -->
-					<div class="booking_cancel">
-						<button class="btn">
-							<span>취소</span>
-						</button>
-					</div>
+					{cancelMessage}
 
 				</div>
 			</div>
--- src/main/webapp/resources/js/myreservation.js
+++ src/main/webapp/resources/js/myreservation.js
@@ -1,33 +1,72 @@
 document.addEventListener("DOMContentLoaded", function(){
-	let pageLoadUnit = new PageLoadUnit();
-	pageLoadUnit.updateReserve();
-	//pageLoadUnit.getReservation(reserveInfo);
+	let reservationEmail = "";
+	let name = "loginCookie=";
+	let cookie = document.cookie.split(';');
+	for(let i = 0; i < cookie.length; i++) {
+		let chunk = cookie[i];
+		if(chunk.indexOf(name)==0) reservationEmail = chunk.substring(name.length, chunk.length);
+	}
+	let pageLoadUnit = new PageLoadUnit(reservationEmail);
 });
 
-function PageLoadUnit() {}
+function PageLoadUnit(email) {
+	this.email = email;
+	this.updateReserve(email);
+}
 PageLoadUnit.prototype = {
-	updateReserve : function() {
-		let name = "loginCookie=";
-		let cookie = document.cookie.split(';');
-		let cookieResult = "";
-		for(let i = 0; i < cookie.length; i++) {
-			let chunk = cookie[i];
-			if(chunk.indexOf(name)==0) cookieResult = chunk.substring(name.length, chunk.length);
-		}
+	updateReserve : function(email) {
 		let oReq = new XMLHttpRequest();
 		oReq.addEventListener("load", function(){
 			let reserveInfo = JSON.parse(oReq.responseText);
-			console.log(reserveInfo);
-			this.getReservation(reserveInfo);
+			let getReservation = new GetReservation();
+
+			getReservation.getReservations(reserveInfo);
 		});
-		oReq.open("GET", "/reservation/api/reservations?reservationEmail=" + cookieResult, true);
-		console.log(cookieResult);
+		oReq.open("GET", "/reservation/api/reservations?reservationEmail=" + email, true);
 		oReq.send();
-	},
+	}
+}	
 	
 	// 예약정보 출력
-	getReservation : function(reserveInfo) {
+function GetReservation () {}	
+GetReservation.prototype = {
+	getReservations : function(reserveInfo) {
 		let template = document.querySelector("#reservationArticle").innerHTML;
 		let bindTemplate = Handlebars.compile(template);
+		reserveInfo.reservations.forEach(function(v){
+			let cancelMessage = this.getCancelMessageInfo(v.cancelFlag, v.reservationDate)[0];
+			let cancelMessageLocation = this.getCancelMessageInfo(v.cancelFlag, v.reservationDate)[1];
+			let data = {
+				"reservationInfoId" : v.reservationInfoId,
+				productDescription : v.displayInfo.productDescription,
+					reservationDate : v.reservationDate.substr(0, v.reservationDate.lastIndexOf(":")),
+					productContent : v.displayInfo.productContent,
+					placeLot : v.displayInfo.placeLot,
+					placeName : v.displayInfo.placeName,
+					totalPrice : v.totalPrice,
+					productId : v.productId
+			}
+			let putLocation = document.querySelector(cancelMessageLocation);
+			let resultHtml = bindTemplate(data);
+			resultHtml = resultHtml.replace(/{cancelMessage}/g, cancelMessage);
+			putLocation.innerHTML += resultHtml;
+			putLocation.style.display = 'block';
+		}.bind(this));
+	},
+	
+	getCancelMessageInfo : function(cancelFlag, reservationDate) {
+		if(cancelFlag == 1) {
+			return ["", "li.card.used.cancel"];
+		}
+		let currentTime = new Date();
+		let reservationTime = new Date(reservationDate);
+		
+		if(currentTime < reservationTime) {
+			return ["<div class='booking_cancel'> <button class='btn'> <span>취소</span> </button> </div>", "li.card.confirmed"];
+		} else {
+			return ["<div class='booking_cancel'> <button class='btn'> <span>예매자 리뷰 남기기</span> </button> </div>", "li.card.used"];
+		}
 	}
-}
+	
+}	
+	
