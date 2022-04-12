c5e3317917b9e90b1f552cd0e5af21c18c8a65d1
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-04-12 19:04:38 +0900

    feat : 예매 리뷰 jsp생성
    이용 완료한 상품에 리뷰를 남길수있게 생성.

--- src/main/java/kr/or/connect/reservation/controller/ReservationController.java
+++ src/main/java/kr/or/connect/reservation/controller/ReservationController.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReservationController {
@@ -31,5 +32,17 @@ public class ReservationController {
 	@GetMapping("/review")
 	public String review() {
 		return "review";
+	}
+//	@GetMapping("/reviewWrite")
+//	public String reviewWrite() {
+//		return "reviewWrite";
+//	}
+	@GetMapping("/reviewWrite")
+	public String reviewWrite(
+			@RequestParam(name="reservationInfoId", required=true) int reservationInfoId,
+			@RequestParam(name="productId", required=true) int productId, ModelMap model) {
+		model.addAttribute("reservationInfoId", reservationInfoId);
+		model.addAttribute("productId", productId);
+		return "reviewWrite";
 	}
 }
