4d3fb7b0873a87da3d781a7c9f1b6b4eda1460e1
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-04-25 23:23:43 +0900

    feat : reviewWrite.js 별점 이벤트
    유저가 별점을 선택하면 그 이벤트를 js로 보내서 등록할수있게 구현.

--- src/main/webapp/resources/js/reviewWrite.js
+++ src/main/webapp/resources/js/reviewWrite.js
@@ -1,3 +1,16 @@
-/**
- * 
- */
+document.addEventListener("DOMContentLoaded", function(){
+	let reviewWritePage = new GetReviewWritePage();
+});
+
+function GetReviewWritePage(){
+	this.stars = document.querySelectorAll(".rating>input.rating_rdo");
+	this.registerEvents();
+}
+GetReviewWritePage.prototype = {
+	// 별점 이벤트 등록
+	registerEvents : function() {
+		this.stars.forEach(function(v){
+			
+		})
+	}
+}
